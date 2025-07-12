b9832ac1f86e944601c8aa61bc93ef80afbf89a0
Noah Lin <nglin34@gmail.com>
2025-07-12 19:19:17 -0400

    install relevant packages

---
+++ app/(profile)/export.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export default function Export() {
+  return (
+    <View>
+      <Text>This will save your health data as a text file</Text>
+    </View>
+  );
+}
